db1623b0654af3cf6357f56d8b49f49533f3600d
Daniel Duff <daniel.duff@aginic.com>
2022-07-29 15:53:06 +1000

    Added gradients to themes

--- src/utils/themes.ts
+++ src/utils/themes.ts
@@ -3,21 +3,25 @@ import { MantineThemeOverride } from '@mantine/core';
 export const darkTheme: MantineThemeOverride = {
   colorScheme: 'dark',
   primaryColor: 'green',
+  defaultGradient: { from: 'green', to: 'teal', deg: 90 },
 };
 
 export const lightTheme: MantineThemeOverride = {
   colorScheme: 'light',
   primaryColor: 'pink',
+  defaultGradient: { from: 'pink', to: 'blue', deg: 90 },
 };
 
 export const redTheme: MantineThemeOverride = {
   colorScheme: 'light',
   primaryColor: 'red',
+  defaultGradient: { from: 'red', to: 'orange', deg: 90 },
 };
 
 export const purpleTheme: MantineThemeOverride = {
   colorScheme: 'dark',
   primaryColor: 'grape',
+  defaultGradient: { from: 'grape', to: 'violet', deg: 90 },
 };
 
 export const defaultTheme: MantineThemeOverride = {
