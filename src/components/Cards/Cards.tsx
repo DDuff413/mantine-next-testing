bfe2887913f342d923d6f012a472cbbf36df13d4
Daniel Duff <daniel.duff@aginic.com>
2022-08-03 16:24:16 +1000

    removed unneccessary code

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,17 +1,12 @@
 import { Group } from '@mantine/core';
 
-import useStyles from 'components/Cards/Cards.styles';
 import { QuestionCard } from 'components/QuestionCard/QuestionCard';
 
-export const Cards = () => {
-  const { classes } = useStyles();
-
-  return (
-    <Group position="center" pt={10}>
-      <QuestionCard />
-      <QuestionCard />
-      <QuestionCard />
-      <QuestionCard />
-    </Group>
-  );
-};
+export const Cards = () => (
+  <Group position="center" pt={10}>
+    <QuestionCard />
+    <QuestionCard />
+    <QuestionCard />
+    <QuestionCard />
+  </Group>
+);
