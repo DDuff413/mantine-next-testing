a1c13295fb62d9251c444f377f03020a5bca7c67
Daniel Duff <daniel.duff@aginic.com>
2022-08-09 17:02:02 +1000

    Added navbar scaling for smaller display

--- src/components/TopNavbar/TopNavbar.styles.ts
+++ src/components/TopNavbar/TopNavbar.styles.ts
@@ -1,17 +1,40 @@
 import { createStyles } from '@mantine/core';
 
-export default createStyles(() => ({
+export default createStyles((theme) => ({
   navbar: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+    [`@media (max-width: ${theme.breakpoints.md}px)`]: {
+      flexDirection: 'column',
+      gap: 20,
+    },
   },
   navbarLinkGroup: {
     flexDirection: 'row',
     flexWrap: 'nowrap',
     gap: 40,
+    [`@media (max-width: ${theme.breakpoints.md}px)`]: {
+      flexDirection: 'column',
+      gap: 20,
+    },
+  },
+  navbarHeadingGroup: {
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    [`@media (max-width: ${theme.breakpoints.md}px)`]: {
+      justifyContent: 'space-between',
+      width: '90vw',
+      paddingTop: 35,
+    },
   },
   navbarAnchor: {
     whiteSpace: 'nowrap',
+    ':hover': { textDecoration: 'none' },
+  },
+  navbarHomeAnchor: {
+    whiteSpace: 'nowrap',
+    ':hover': { textDecoration: 'none' },
+    fontSize: 24,
   },
 }));
--- src/components/TopNavbar/TopNavbar.tsx
+++ src/components/TopNavbar/TopNavbar.tsx
@@ -1,22 +1,65 @@
-import { Anchor, Group, Navbar } from '@mantine/core';
+import { Anchor, Burger, Group, Navbar } from '@mantine/core';
+import { useState } from 'react';
 
 import useStyles from 'components/TopNavbar/TopNavbar.styles';
 
 export const TopNavbar = () => {
   const { classes } = useStyles();
 
+  const [expand, setExpand] = useState(true);
+
   return (
-    <Navbar className={classes.navbar} height={100} px={80}>
+    <Navbar
+      className={classes.navbar}
+      height={100}
+      px={80}
+      sx={(theme) => ({
+        [`@media (max-width: ${theme.breakpoints.md}px)`]: {
+          height: expand ? 370 : 100,
+          paddingBottom: expand ? 30 : 0,
+        },
+      })}
+    >
       <Navbar.Section>
-        <Anchor href="/">...CAT</Anchor>
+        <Group className={classes.navbarHeadingGroup}>
+          <Anchor href="/" className={classes.navbarHomeAnchor}>
+            ...CAT
+          </Anchor>
+          <Burger
+            opened={expand}
+            onClick={() => setExpand(!expand)}
+            sx={(theme) => ({
+              [`@media (min-width: ${theme.breakpoints.md}px)`]: {
+                display: 'none',
+              },
+            })}
+          />{' '}
+        </Group>
       </Navbar.Section>
       <Navbar.Section>
-        <Group className={classes.navbarLinkGroup}>
-          <Anchor href="/cards">Cards</Anchor>
-          <Anchor href="/progress">Progress</Anchor>
-          <Anchor href="/pagination">Pagination</Anchor>
-          <Anchor href="/results">Results</Anchor>
-          <Anchor href="/multiselect">MultiSelect</Anchor>
+        <Group
+          className={classes.navbarLinkGroup}
+          sx={(theme) => ({
+            [`@media (max-width: ${theme.breakpoints.md}px)`]: {
+              display: expand ? 'flex' : 'none',
+            },
+          })}
+        >
+          <Anchor href="/cards" className={classes.navbarAnchor}>
+            Cards
+          </Anchor>
+          <Anchor href="/progress" className={classes.navbarAnchor}>
+            Progress
+          </Anchor>
+          <Anchor href="/pagination" className={classes.navbarAnchor}>
+            Pagination
+          </Anchor>
+          <Anchor href="/results" className={classes.navbarAnchor}>
+            Results
+          </Anchor>
+          <Anchor href="/multiselect" className={classes.navbarAnchor}>
+            MultiSelect
+          </Anchor>
           <Anchor href="/topic" className={classes.navbarAnchor}>
             Topic Card
           </Anchor>
